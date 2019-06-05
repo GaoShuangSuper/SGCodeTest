bec4fadf30fb7dfa3796011077c808e64a2121a9
GaoShuangSuper <41044961+GaoShuangSuper@users.noreply.github.com>
2019-06-05 11:43:44 +0800

    Lambda expression
    For net core

---
+++ LamdaNetCore/LamdaNetCore/DelegatesClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LamdaNetCore
+{
+    //https://www.w3cschool.cn/csharp/csharp-delegates.html
+    public class DelegatesClass
+    {
+        delegate void TestDelegate(string s);
+        public void M(string s)
+        {
+            Console.WriteLine(s);
+        }
+        public void Main()
+        {
+            //original 
+            TestDelegate tsA = new TestDelegate(M);
+            //C#2.0
+            TestDelegate tsB = delegate (string s) { Console.WriteLine(s); };
+            //C# 3.0
+            TestDelegate tsC = (s) => { Console.WriteLine(s); };
+        }
+    }
+}
---
+++ LamdaNetCore/LamdaNetCore/LamdaTESTClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LamdaNetCore
+{
+    public class LamdaTESTClass
+    {
+        public void Method1()
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            dictionary.Add("1", "a");
+            dictionary.Add("2", "b");
+            dictionary.Add("3", "c");
+            dictionary.Add("4", "d");
+            foreach (string key in dictionary.Keys)
+            {              
+                Console.WriteLine(key,dictionary[key]);
+            }          
+        }
+    }
+}
